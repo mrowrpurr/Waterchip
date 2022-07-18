317070034d3238cd3ee5ac9cd708d085a4b5c895
Mrowr Purr <mrowr.purr@gmail.com>
2022-07-18 15:54:50 -0700

    Naming things.

--- scripts/headers/Waterchip.h
+++ scripts/headers/Waterchip.h
@@ -9,4 +9,24 @@ import variable __waterchip_data;
 // Workaround for SSLC bug: Map accessors don't work on import/export variables.
 variable waterchip_data;
 
+// Stores the name provided via describe("") or context("") or test_suite("")
+variable __waterchip__testsuite_name = "Tests";
+
+// Reference to the data map for this test suite (in waterchip_data.test_suites)
+variable __waterchip_testsuite_data;
+
+// Tracks if script has been configured to run itself (in 1 frame) after setup
+variable __waterchip_testsuite_repeat_initialized = false;
+
+// Name of the currentlt running test
+variable __waterchip_testsuite_currently_running_test_name;
+
+// Whether or not the script is currently running tests
+// Set to true when it detects it's running via repeat (1 frame after setup)
+// After which the repeat is disabled for the script
+variable __waterchip_testsuite_currently_running_tests;
+
+// A little bit of unnecessary backup protection in cast the script can be
+// invoked via repeat when it's already running. I don't think this is needed.
+variable __waterchip__testsuite_times_run_tests_backup_test_var_to_verify_single_run = 0;
 

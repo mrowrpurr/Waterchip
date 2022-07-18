9a104aacbe97ccc2c752ee39ce8995f94132fee6
Mrowr Purr <mrowr.purr@gmail.com>
2022-07-18 14:43:27 -0700

    Setup Waterchip.h

---
+++ scripts/headers/Waterchip.h
@@ -0,0 +1,12 @@
+/*
+    Library for Waterchip test/spec framework ~ by Mrowr Purr ~
+*/
+
+// Imported variable for Waterchip test runner.
+import variable __waterchip_data;
+
+// Local variable for accessing ___waterchip_data using Map accessors.
+// Workaround for SSLC bug: Map accessors don't work on import/export variables.
+variable waterchip_data;
+
+

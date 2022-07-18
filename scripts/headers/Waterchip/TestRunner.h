0d2317762945805b5ef31508d0810690b4f03c73
Mrowr Purr <mrowr.purr@gmail.com>
2022-07-18 14:43:42 -0700

    Setup Waterchip test runner

---
+++ scripts/headers/Waterchip/TestRunner.h
@@ -0,0 +1,14 @@
+/*
+    Test Runner for Waterchip test/spec framework ~ by Mrowr Purr ~
+*/
+
+#define DEBUG_NAME "Waterchip"
+
+#include "Waterchip/DebugHelper.h"
+#include "Waterchip/TestData.h"
+#include "Waterchip/TestReporter.h"
+
+inline procedure TestRunner_Run begin
+    debug("loading...");
+    call TestData_Initialize;
+end

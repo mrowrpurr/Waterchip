494144e5e7cf3a4718caee50554943e4418ae687
Mrowr Purr <mrowr.purr@gmail.com>
2022-07-31 00:46:26 -0700

    Support for include/exclude tests via Waterchip.ini

--- scripts/headers/Waterchip.h
+++ scripts/headers/Waterchip.h
@@ -43,16 +43,16 @@
 #include "sfall/lib.arrays.h"
 #include "sfall/lib.strings.h"
 
-// Imported variable for Waterchip test runner.
-import variable __waterchip_data;
-
 procedure start;
 
-// Local variable for accessing ___waterchip_data using Map accessors.
-// Workaround for SSLC bug: Map accessors don't work on import/export variables.
-variable waterchip_data;
+// Path to Waterchip configuration file 
+#define WATERCHIP_INI                 "mods/Waterchip.ini"
+#define WATERCHIP_INI_ENABLED_SETTING (WATERCHIP_INI + "|Waterchip|enabled")
 
-// Reference to the data map for this test suite (in waterchip_data.test_suites)
+// Stores the configuration read from the .ini file, when present
+variable __waterchip_config;
+
+// Stores data about the test suite for the runner, e.g. all test names
 variable __waterchip_testsuite_data;
 
 // Tracks if script has been configured to run itself (in 1 frame) after setup
@@ -105,7 +105,7 @@ variable __waterchip_testsuite_total_skipped;
 // Cleqr the prefix for Watership debug output messages
 #define clear_waterchip_debug_prefix __waterchip_output_prefix = ""
 
-// Test state names stored in waterchip_data.test_suites[suite].test_results
+// Test state names stored in __waterchip_data.testsuites[suite].test_results
 #define WATERCHIP_TEST_RESULT_FAILED  "FAIL"
 #define WATERCHIP_TEST_RESULT_PASSED  "PASS"
 #define WATERCHIP_TEST_RESULT_SKIPPED "SKIP"
@@ -116,8 +116,8 @@ variable __waterchip_testsuite_total_skipped;
 // TODO - do NOT use sprintf_array (so that people can use sprintf_array)
 //        you cannot sprintf_array inside of a sprintf_array
 //
-#define __waterchip_debug_test_suite(text) debug_msg(sprintf_array("%s[%s] %s\n",        [__waterchip_output_prefix, __waterchip__testsuite_name, text]))
-#define __waterchip_debug_test(text)       debug_msg(sprintf_array("%s[%s] [%s] [%s]\n", [__waterchip_output_prefix, __waterchip__testsuite_name, __waterchip_testsuite_currently_running_test_name, text]))
+#define __waterchip_debug_testsuite(text) debug_msg(sprintf_array("%s[%s] %s\n",        [__waterchip_output_prefix, __waterchip__testsuite_name, text]))
+#define __waterchip_debug_test(text)      debug_msg(sprintf_array("%s[%s] [%s] [%s]\n", [__waterchip_output_prefix, __waterchip__testsuite_name, __waterchip_testsuite_currently_running_test_name, text]))
 
 // Print a message to the debug output (in the same format as Waterchip)
 #define print(text) \
@@ -125,7 +125,7 @@ variable __waterchip_testsuite_total_skipped;
         if __waterchip_testsuite_currently_running_test_name then \
             __waterchip_debug_test(text); \
         else \
-            __waterchip_debug_test_suite(text); \
+            __waterchip_debug_testsuite(text); \
     else \
         debug_msg(__waterchip_output_prefix + " [] [] " + sprintf("[%s]", text))
 
@@ -137,11 +137,11 @@ variable __waterchip_testsuite_total_skipped;
 ************************************/
 
 // variable __waterchip__testsuite_name (created by the describe macro)
-// Stores the name provided via describe("") or context("") or test_suite("")
+// Stores the name provided via describe("") or context("") or testsuite("")
 //
 // Setup a test suite (creates the start procedure under the covers)
-#define describe(test_suite_name) \
-    variable __waterchip__testsuite_name = test_suite_name; \
+#define describe(testsuite_name) \
+    variable __waterchip__testsuite_name = testsuite_name; \
     procedure start
 
 // Runs a block of code *once* (when the first test is executed)
@@ -161,9 +161,6 @@ variable __waterchip_testsuite_total_skipped;
 
 // BDD-ish alias for the xUnit test()
 #define it(example_name) test(example_name)
-
-// BDD-ish alias for the xUnit test()
-#define example(example_name) test(example_name)
 
 #define skip(test_name) \
     __waterchip_testsuite_currently_defining_skipped_test = true; \
@@ -222,6 +219,16 @@ variable __waterchip_testsuite_total_skipped;
 #define not_to_equal(expected) \
     if __waterchip_testsuite_current_expect_value == expected then \
         fail("Expected not to equal.\nExpected: '" + expected + "'\nActual: '" + __waterchip_testsuite_current_expect_value + "'")
+
+// Does a string start with something?
+// #define to_start_with(expected) \
+//     switch typeof(__waterchip_testsuite_current_expect_value) begin \
+//         case 1: fail(); \
+//         case 2: fail("to_be_empty called with a float (invalid argument)"); \
+//         case 3: if strlen(__waterchip_testsuite_current_expect_value) > 0 then \
+//             fail(sprintf("Expected string to be empty, but was \"%s\"", __waterchip_testsuite_current_expect_value)); \
+//     end \
+//     false
        
 // Helper for asserting 0 value
 #define to_be_zero     to_equal(0)
@@ -266,12 +273,12 @@ variable __waterchip_testsuite_total_skipped;
         case 1: begin \
             if __waterchip_testsuite_current_expect_value then begin \
                 if len_array(__waterchip_testsuite_current_expect_value) == -1 then \
-                    fail("to_be_empty called on int which is not an array"); \
+                    fail("not_to_be_empty called on int which is not an array"); \
                 if len_array(__waterchip_testsuite_current_expect_value) == 0 then \
                     fail("Expected array not to be empty, but had 0 elements"); \
             end \
         end \
-        case 2: fail("to_be_empty called with a float (invalid argument)"); \
+        case 2: fail("not_to_be_empty called with a float (invalid argument)"); \
         case 3: if strlen(__waterchip_testsuite_current_expect_value) == 0 then \
             fail("Expected string not to be empty, but had 0 length"); \
     end \
@@ -287,22 +294,24 @@ variable __waterchip_testsuite_total_skipped;
 
 // Shared code between `test` and `todo`
 #define __waterchip_test_macro_start(test_name) \
-    if not waterchip_data then waterchip_data = __waterchip_data; \
     if not __waterchip_testsuite_data then begin \
-        __waterchip_debug_test_suite(""); \
+        call __waterchip_load_config; \
         __waterchip_testsuite_data = {}; \
         fix_array(__waterchip_testsuite_data); \
-        waterchip_data[__waterchip__testsuite_name] = __waterchip_testsuite_data; \
         __waterchip_testsuite_data.test_names = []; \
         fix_array(__waterchip_testsuite_data.test_names); \
         __waterchip_testsuite_data.test_results = {}; \
         fix_array(__waterchip_testsuite_data.test_results); \
     end \
+    if not __waterchip_config.enabled then return; \
+    if not __waterchip_should_run_testsuite(__waterchip__testsuite_name) then return; \
     if (not __waterchip_testsuite_currently_running_tests) and scan_array(__waterchip_testsuite_data.test_names, test_name) > -1 and __waterchip__testsuite_times_run_tests_backup_test_var_to_verify_single_run == 0 then begin \
         set_global_script_repeat(0); \
         __waterchip__testsuite_times_run_tests_backup_test_var_to_verify_single_run++; \
         __waterchip_testsuite_currently_running_tests = true; \
         foreach __waterchip_testsuite_currently_running_test_name in (__waterchip_testsuite_data.test_names) begin \
+            if not __waterchip_should_run_test(__waterchip_testsuite_currently_running_test_name) then \
+                __waterchip_testsuite_data.test_results[__waterchip_testsuite_currently_running_test_name].status = WATERCHIP_TEST_RESULT_SKIPPED; \
             if __waterchip_testsuite_data.test_results[__waterchip_testsuite_currently_running_test_name].status != WATERCHIP_TEST_RESULT_SKIPPED then begin \
                 call start; \
                 __waterchip_testsuite_first_test_run = false; \
@@ -321,9 +330,9 @@ variable __waterchip_testsuite_total_skipped;
             end \
         end \
         if __waterchip_testsuite_total_failed > 0 then \
-            __waterchip_debug_test_suite("[Tests failed] [" + __waterchip_testsuite_total_failed + " failed, " + __waterchip_testsuite_total_passed + " passed, " + __waterchip_testsuite_total_skipped + " skipped]"); \
+            __waterchip_debug_testsuite("[Tests failed] [" + __waterchip_testsuite_total_failed + " failed, " + __waterchip_testsuite_total_passed + " passed, " + __waterchip_testsuite_total_skipped + " skipped]"); \
         else \
-            __waterchip_debug_test_suite("[Tests passed] [" + __waterchip_testsuite_total_passed + " passed, " + __waterchip_testsuite_total_skipped + " skipped]"); \
+            __waterchip_debug_testsuite("[Tests passed] [" + __waterchip_testsuite_total_passed + " passed, " + __waterchip_testsuite_total_skipped + " skipped]"); \
         return; \
     end \
     if not __waterchip_testsuite_repeat_initialized then begin \
@@ -341,17 +350,126 @@ variable __waterchip_testsuite_total_skipped;
     end
 
 /****************************************
-  Aliases (to move to another #include)
+     Configuration from Waterchip.ini
 *****************************************/
 
-// #define after teardown
-// #define after_each teardown
+procedure __waterchip_load_config begin
+    if __waterchip_config then return;
 
-// #define before setup
-// #define before_each setup
+    __waterchip_config = {};
+    fix_array(__waterchip_config);
+    
+    __waterchip_config.enabled = get_ini_setting(WATERCHIP_INI_ENABLED_SETTING);
 
-// // xUnit-ish alias for the BDD describe()
-// #define context(test_suite_name) describe(test_suite_name)
+    // -1 means the .ini file (or section/key) was not found
+    if __waterchip_config.enabled == -1 then
+        __waterchip_config.enabled = false; // we don't run tests if no .ini
 
-// // xUnit-ish alias for the BDD describe()
-// #define test_suite(test_suite_name) describe(test_suite_name)
+    // if either the .ini wasn't found or not enabled, return without reading config.
+    if not __waterchip_config.enabled then return;
+
+    // Read test suite and test filters...
+
+    // [TestSuites:Include]
+    __waterchip_config.include_testsuite_names = get_ini_section(WATERCHIP_INI, "TestSuites:Include");
+    if not __waterchip_config.include_testsuite_names then __waterchip_config.include_testsuite_names = {};
+    fix_array(__waterchip_config.include_testsuite_names);
+
+    // [TestSuites:Exclude]
+    __waterchip_config.exclude_testsuite_names = get_ini_section(WATERCHIP_INI, "TestSuites:Exclude");
+    if not __waterchip_config.exclude_testsuite_names then __waterchip_config.exclude_testsuite_names = {};
+    fix_array(__waterchip_config.exclude_testsuite_names);
+
+    // [Tests:Include]
+    __waterchip_config.include_test_names = get_ini_section(WATERCHIP_INI, "Tests:Include");
+    if not __waterchip_config.include_test_names then __waterchip_config.include_test_names = {};
+    fix_array(__waterchip_config.include_test_names);
+
+    // [Tests:Exclude]
+    __waterchip_config.exclude_test_names = get_ini_section(WATERCHIP_INI, "Tests:Exclude");
+    if not __waterchip_config.exclude_test_names then __waterchip_config.exclude_test_names = {};
+    fix_array(__waterchip_config.exclude_test_names);
+
+    // Read [Output] configuration
+    __waterchip_config.output = get_ini_section(WATERCHIP_INI, "Output");
+    if not __waterchip_config.output then __waterchip_config.output = {};
+    fix_array(__waterchip_config.output);
+end
+
+procedure __waterchip_should_run_testsuite(variable testsuite_name) begin
+    variable filter, enabled, included, excluded;
+    variable includes_defined = (len_array(__waterchip_config.include_testsuite_names) > 0);
+    variable excludes_defined = (len_array(__waterchip_config.exclude_testsuite_names) > 0);
+
+    testsuite_name = string_tolower(testsuite_name);
+
+    if includes_defined and excludes_defined then begin
+        foreach filter: enabled in (__waterchip_config.include_testsuite_names) begin
+            if is_in_string(testsuite_name, string_tolower(filter)) then begin
+                included = atoi(enabled);
+                break;
+            end
+        end
+        foreach filter: enabled in (__waterchip_config.exclude_testsuite_names) begin
+            if is_in_string(testsuite_name, string_tolower(filter)) then begin
+                excluded = atoi(enabled);
+                break;
+            end
+        end
+        return included and not excluded;
+    end else if includes_defined then begin
+        foreach filter: enabled in (__waterchip_config.include_testsuite_names) begin
+            if is_in_string(testsuite_name, string_tolower(filter)) then begin
+                return atoi(enabled);
+            end
+        end
+        return false;
+    end else if excludes_defined then begin
+        foreach filter: enabled in (__waterchip_config.exclude_testsuite_names) begin
+            if is_in_string(testsuite_name, string_tolower(filter)) then begin
+                return not atoi(enabled);
+            end
+        end
+        return true;
+    end
+    return true;
+end
+
+procedure __waterchip_should_run_test(variable test_name) begin
+    variable filter, enabled, included, excluded;
+    variable includes_defined = (len_array(__waterchip_config.include_test_names) > 0);
+    variable excludes_defined = (len_array(__waterchip_config.exclude_test_names) > 0);
+
+    test_name = string_tolower(test_name);
+
+    if includes_defined and excludes_defined then begin
+        foreach filter: enabled in (__waterchip_config.include_test_names) begin
+            if is_in_string(test_name, string_tolower(filter)) then begin
+                included = atoi(enabled);
+                break;
+            end
+        end
+        foreach filter: enabled in (__waterchip_config.exclude_test_names) begin
+            if is_in_string(test_name, string_tolower(filter)) then begin
+                excluded = atoi(enabled);
+                break;
+            end
+        end
+        return included and not excluded;
+    end else if includes_defined then begin
+        foreach filter: enabled in (__waterchip_config.include_test_names) begin
+            if is_in_string(test_name, string_tolower(filter)) then begin
+                return atoi(enabled);
+            end
+        end
+        return false;
+    end else if excludes_defined then begin
+        foreach filter: enabled in (__waterchip_config.exclude_test_names) begin
+            if is_in_string(test_name, string_tolower(filter)) then begin
+                return not atoi(enabled);
+            end
+        end
+        return true;
+    end
+    return true;
+end

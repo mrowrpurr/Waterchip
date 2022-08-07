22b1b4d52c13ede7848f4d3a5a69d56dc5ab543d
Mrowr Purr <mrowr.purr@gmail.com>
2022-08-07 11:00:58 -0700

    Setup basics for it/xit/todo/setup/teardown

--- scripts/headers/Waterchip_v2.h
+++ scripts/headers/Waterchip_v2.h
@@ -9,30 +9,46 @@
 ******************************************/
 
 #define describe(test_suite_name) \
-    procedure __waterchip_tests(variable current_test); \
+    procedure __waterchip_tests(variable defining_tests, variable current_test, variable run_test, variable run_setup, variable run_teardown); \
     procedure start begin \
         call __waterchip_initialize(test_suite_name); \
-        call __waterchip_tests(0); \
+        call __waterchip_tests(true, "", false, false, false); \
         variable test_count = len_array(__waterchip_data.test_results); \
         if test_count then begin \
-            test_output(sprintf("[%s tests defined]", test_count)); \
+            variable test_result; \
+            foreach test_result in (__waterchip_data.test_results) begin \
+                variable test_name = test_result.name; \
+                if __waterchip_data.has_setup then call __waterchip_tests(false, test_name, false, true, false); \
+                call __waterchip_tests(false, test_name, true, false, false); \
+                if __waterchip_data.has_teardown then call __waterchip_tests(false, test_name, false, false, true); \
+            end \
+            test_output(sprintf("[Ran %s tests]", test_count)); \
         end else begin \
             test_output("[No tests defined]"); \
         end \
     end \
-    procedure __waterchip_tests(variable current_test)
+    procedure __waterchip_tests(variable defining_tests, variable current_test, variable run_test, variable run_setup, variable run_teardown)
 
-#define it(test_name)
+#define it(test_name) \
+    if defining_tests then call __waterchip_declare_test(test_name); \
+    if run_test and current_test == test_name then
 
-#define xit(test_name)
+#define xit(test_name) \
+    if defining_tests then call __waterchip_declare_test(test_name, true); \
+    if false then
 
-#define todo(test_name)
+#define todo(test_name) \
+    if defining_tests then call __waterchip_declare_test(test_name, true)
 
 #define setup_once
 
-#define setup
+#define setup \
+    if defining_tests then __waterchip_data.has_setup = true; \
+    if run_setup then
 
-#define teardown
+#define teardown \
+    if defining_tests then __waterchip_data.has_teardown = true; \
+    if run_teardown then
 
 /******************************************
     Script Variables, global to script
@@ -76,7 +92,7 @@ procedure __waterchip_initialize(variable test_suite_name) begin
     end
 end
 
-procedure __waterchip_declare_test(variable test_name) begin
+procedure __waterchip_declare_test(variable test_name, variable skip = false) begin
     variable test_result = { "name": test_name };
     fix_array(test_result);
     variable test_count = len_array(__waterchip_data.test_results);
